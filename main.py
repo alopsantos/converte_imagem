d9152988649268f58c408f07cbf3fd2d7bc36f29
alopsantos <anderson_lops@hotmail.com>
2023-07-30 23:33:33 -0300

    :hammer: Correções e ajuestes

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from PIL import Image
 import os
 
-url = input("Informe o caminho das imagens: ")
+url = input("Informe o caminho completo da pasta onde salvou as imagens: ")
 
 arquivos = os.listdir(url)
-extensoes_in = input("Infome a extensao das imagens: ")  # .png
-extensoes_out = input("Infome a extensao para qual quer converter: ")  # .bmp
-# file_in = ""
-# img = Image.open(file_in)
-
-# file_out = ""
-# img.save(file_out)
+print("Informe a extensão das imagens na pasta, não precisa informar o ponto")
+extensoes_in = input(
+    "exemplo (jpg, webp, png, bmp): ")  # .png
+print("Informe a extensão que deseja transformar, não precisa informar o ponto")
+extensoes_out = input(
+    "Exemplo (jpg, webp, png, bmp): ")  # .bmp
 
 for i in arquivos:
+
     if extensoes_in == []:
         print(i)
 
     else:
         extensao = i.split('.')[-1]
         if extensao in extensoes_in:
-            file_in = (url+i)
+            file_in = (url+'/'+i)
             img = Image.open(file_in)
-            file_out = url + i.split('.')[0]+extensoes_out
+            file_out = url + '/' + i.split('.')[0]+'.'+extensoes_out
             img.save(file_out)
-
-
-# print(pasta)
